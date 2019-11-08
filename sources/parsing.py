eab5a99da796f094bbfbe8b29fdeaaa1262f50b1
Sophia K <kholod@ucu.edu.ua>
2019-11-08 14:38:06 +0200

    fixed small bugs in parsing.py; added more documentation

--- sources/parsing.py
+++ sources/parsing.py
@@ -27,22 +27,33 @@ def parse_date(date):
 
 
 def parse_date_range(date_from=None, date_to=None):
-    """Set correct date range"""
-    date_to = parse_date(date_to) if date_to else datetime.datetime.utcnow()  # if not specified, set to current date
+    """Set correct date range
+    If date_to is None, set to current date
+    If date_from is None, set to date_to - 1 year
+    :param date_from: string in format YYYY-MM-DD
+    :param date_to: string in format YYYY-MM-DD
+    :return: datetime.datetime object if date_to and date_from are correct; otherwise - None, None
+    """
+    date_to = parse_date(date_to) if date_to is not None else datetime.datetime.utcnow()  # if not specified, set to current date
     if date_to is None:
-        return date_from, date_to
-    date_from = parse_date(date_from) if date_from else date_to - relativedelta(years=1)
-    if all([date_to, date_from]) and date_to < date_from:
+        return None, None
+    date_from = parse_date(date_from) if date_from is not None else date_to - relativedelta(years=1)
+    if not all([date_to, date_from]) or (date_to < date_from):
         return None, None
     return date_from, date_to
 
 
 def parse_events(events, surname=None):
+    """
+    :param events: list of dictionaries that has keys 'summary' and 'start'
+    :param surname: mentor's surname
+    :return: list of statistics
+    """
     def parse_one_mentor(m_events, last_name):
         first_event, last_event = parse_date(m_events[0]['start']), parse_date(m_events[-1]['start'])
         return {'surname': last_name, 'hours': len(m_events) / 2,
-                'from': f'{first_event.year}-{first_event.month}-{first_event.day}',
-                'to': f'{last_event.year}-{last_event.month}-{last_event.day}'}
+                'from': first_event.strftime('%Y-%m-%d'),
+                'to': last_event.strftime('%Y-%m-%d')}
 
     start = "Mentoring: "
     start_length = len(start)
