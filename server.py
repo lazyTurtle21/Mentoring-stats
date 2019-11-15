5e06d22a8576559f72250b1d9704df39e1343a10
Sophia K <kholod@ucu.edu.ua>
2019-11-15 15:06:07 +0200

    added logo on all pages

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, url_for, redirect
 from sources.api import app
 
 app.config['JSON_AS_ASCII'] = False
@@ -8,6 +8,11 @@ app.config['CREDS'] = 'credentials.json'
 @app.route('/')
 def main_page():
     return render_template('index.html')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return redirect(url_for('static', filename='img/ralabs_logo.png'))
 
 
 if __name__ == '__main__':
